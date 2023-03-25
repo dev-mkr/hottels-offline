5bd65839817eb3fa4c583e015d55e8f370ae010b
dev-mkr <webdev.mu@gamil.com>
2023-03-25 15:42:48 +0200

    update: vite config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    middleware: [
-      (req, res, next) => {
-        if (req.method === "GET" && !req.path.startsWith("/api")) {
-          res.sendFile(resolve(__dirname, "dist", "index.html"));
-        } else {
-          next();
-        }
-      },
-    ],
-  },
 });
