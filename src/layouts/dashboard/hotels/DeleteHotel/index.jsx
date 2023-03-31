f50de1ebecdbf7adc0c813653356b185c7e311f3
Mohamed Kharashy <webdev.mu@gmail.com>
2023-03-31 00:44:34 +0000

    Finished delte hotel

---
+++ src/layouts/dashboard/hotels/DeleteHotel/index.jsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import { Button, Typography, Modal, Alert, Card } from "@mui/material";
+import axios from "src/api/axios";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 600,
+  maxHeight: 200,
+  bgcolor: "",
+  boxShadow: 24,
+  p: 2,
+  borderRadius: "15px",
+  overflow: "auto",
+  display: "flex",
+  columnGap: "10px",
+  justifyContent: "space-between",
+};
+
+const DeleteHotel = ({ hotelName, hotelId, token }) => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const confirmDelete = () => {
+    let data = new FormData();
+    data.append("_method", "DELETE");
+    axios
+      .request({
+        method: "POST",
+        url: `/api/admin/hotels/${hotelId}`,
+        headers: {
+          "content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        data: data,
+      })
+      .then((res) => res?.status === 200 && handleClose());
+  };
+
+  return (
+    <>
+      <Button variant="contained" color="error" onClick={handleOpen}>
+        Archive
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Card sx={style}>
+          <Alert variant="outlined" severity="error">
+            Are you sure you want to Archive {hotelName}?
+          </Alert>
+          <Button variant="contained" color="error" onClick={confirmDelete}>
+            Archive
+          </Button>
+        </Card>
+      </Modal>
+    </>
+  );
+};
+export default DeleteHotel;
